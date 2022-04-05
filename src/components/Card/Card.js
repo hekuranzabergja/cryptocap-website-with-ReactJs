212374d023b58ad7d3a79ccbe074b6162ee53862
Hekuran Zabergja <hekurannzabergja@gmail.com>
2022-04-05 23:47:01 +0200

    card add

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,34 +1,37 @@
 import React from "react";
 import "./Card.css";
 import { CardData } from "./CardData";
-// import usd from '../Images/usd.png'
-
-
-
+import {AiOutlineArrowUp} from"react-icons/ai"
 const Card = (props) => {
   return (
-  <div>
+    <div>
       <p>Market Trade</p>
-        <div className="card0" >
-      {CardData.map((item, index) => {
-        return (
-          <div className="card">
-               <img src={props.img}  alt=""/>
-            <div key={index}>
-            <p>{item.shortname}</p>
+      <div className="card0">
+        {CardData.map((item, index) => {
+          return (
+            <div className="card">
+              <div className="cryptoname">
+                <img src={item.img} alt={item.name}></img>
+                <p>{item.shortname}</p>
+                <p>{item.name}</p>
+               <div className="crypto-icon-state">
+               <AiOutlineArrowUp/>
+               </div>
+              </div>
+              <div className="break">
+                <hr/>
+              </div>
+              <div className="cryptovalue">
+              <p>{item.value}</p>
+              <img src={item.chart} alt={item.name}></img>
 
-              <p>{item.name}</p>
-                <p>{item.value}</p>
+              <p>{item.percentage}</p>
+              </div>
             </div>
-            <div>
-            <img src={item.img}  alt=""/>
-
-            </div>
-          </div>
-        );
-      })}
+          );
+        })}
+      </div>
     </div>
-  </div>
   );
 };
 
--- src/components/Card/CardData.js
+++ src/components/Card/CardData.js
@@ -1,32 +1,41 @@
 export const CardData = [
   {
     img: require("../Images/bitcoin.png"),
+    chart: require("../Images/chart.png"),
+
     shortname: "BTC",
     name: "Bitcoin",
     value: "$56,623.54",
+    percentage: "1.43%",
   },
 
   {
     img: require("../Images/etherium.png"),
+    chart: require("../Images/chart.png"),
 
     shortname: "ETH",
     name: "Ethereum",
     value: "$56,623.54",
+    percentage: "1.43%",
   },
 
   {
     img: require("../Images/coin-icon.png"),
+    chart: require("../Images/chart.png"),
 
     shortname: "BNB",
     name: "Binance",
     value: "$56,623.54",
+    percentage: "1.43%",
   },
 
   {
     img: require("../Images/usd.png"),
+    chart: require("../Images/chart.png"),
 
     shortname: "USDT",
     name: "Tether",
     value: "$56,623.54",
+    percentage: "1.43%",
   },
 ];
