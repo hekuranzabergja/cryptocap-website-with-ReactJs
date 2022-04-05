f216d711fc8d66f4652e7e5fdf79ed6a2c8e1939
Hekuran Zabergja <hekurannzabergja@gmail.com>
2022-04-05 16:03:08 +0200

    adding features

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import Header from "./components/Header/Header";
+import Card from "./components/Card/Card";
 
 const App = () => {
   return (
     <div>
       <Navbar />
       <Header />
+      <Card/>
     </div>
   );
 };
