3a32ecfd8996bd9d2051adc8c415f6ad32c6cf4a
Hekuran Zabergja <hekurannzabergja@gmail.com>
2022-04-05 23:11:37 +0200

    update some things

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,20 +1,34 @@
 import React from "react";
 import "./Card.css";
 import { CardData } from "./CardData";
+// import usd from '../Images/usd.png'
+
+
 
 const Card = (props) => {
   return (
-    <div className="card0" >
+  <div>
+      <p>Market Trade</p>
+        <div className="card0" >
       {CardData.map((item, index) => {
         return (
           <div className="card">
+               <img src={props.img}  alt=""/>
             <div key={index}>
+            <p>{item.shortname}</p>
+
               <p>{item.name}</p>
+                <p>{item.value}</p>
+            </div>
+            <div>
+            <img src={item.img}  alt=""/>
+
             </div>
           </div>
         );
       })}
     </div>
+  </div>
   );
 };
 
--- src/components/Card/CardData.js
+++ src/components/Card/CardData.js
@@ -1,32 +1,32 @@
-export const CardData =[
-   {
-    img: 'Images/usd.png',
+export const CardData = [
+  {
+    img: require("../Images/bitcoin.png"),
     shortname: "BTC",
     name: "Bitcoin",
-    value: "$56,623.54"
-   },
+    value: "$56,623.54",
+  },
 
-   {
-    img: 'Images/usd.png',
-    shortname: "BTC",
-    name: "Bitcoin",
-    value: "$56,623.54"
+  {
+    img: require("../Images/etherium.png"),
 
-   },
+    shortname: "ETH",
+    name: "Ethereum",
+    value: "$56,623.54",
+  },
 
-   {
-    img: 'Images/usd.png',
-    shortname: "BTC",
-    name: "Bitcoin",
-    value: "$56,623.54"
+  {
+    img: require("../Images/coin-icon.png"),
 
-   },
+    shortname: "BNB",
+    name: "Binance",
+    value: "$56,623.54",
+  },
 
-   {
-    img: 'Images/usd.png',
-    shortname: "BTC",
-    name: "Bitcoin",
-    value: "$56,623.54"
+  {
+    img: require("../Images/usd.png"),
 
-   },
-]
+    shortname: "USDT",
+    name: "Tether",
+    value: "$56,623.54",
+  },
+];
